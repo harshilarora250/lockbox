29ff6db4f4b1d5b90f6098c2828efc401513f3d7
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-08-02 19:51:00 +0400

    txt as btn

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const { animate, hover, scroll } = Motion;
+const titlegit = document.getElementById("title-git")
+
+titlegit.addEventListener("click", (e) => {
+    window.open("https://github.com/harshilarora250/lockbox/", "_blank");
+})
