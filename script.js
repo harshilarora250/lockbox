c2ed2aad985a94a9b781e452f22570c1ecbcb202
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-08-06 23:00:29 +0400

    input

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const titlegit = document.getElementById("title-git")
 titlegit.addEventListener("click", (e) => {
     window.open("https://github.com/harshilarora250/lockbox/", "_blank");
 })
+
+
+
+
+
+// <ANIMATIONS-BEGIN-HERE>
+// <ANIMATIONS-END-HERE>
