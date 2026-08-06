26f7cafcd4d59be44a64d1170111b64e2a868906
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-08-07 00:17:17 +0400

    saved user algo

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const { animate, hover, scroll } = Motion;
 const titlegit = document.getElementById("title-git") // Lead title text to github repo
 const newpass = document.getElementById("create-pass")
 const passmodal = document.getElementById("passmodal")
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const saveuser = document.getElementById("savepass")
 
 //Lead title text to github repo
 titlegit.addEventListener("click", (e) => {
@@ -13,6 +16,22 @@ titlegit.addEventListener("click", (e) => {
 newpass.addEventListener("click", (e) => {
     passmodal.hidden = false;
 })
+
+saveuser.addEventListener("click", () => {
+    const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+    accounts.push({
+        username: username.value,
+        password: password.value,
+        createdAt: Date.now() // Unix timestamp
+    });
+
+    localStorage.setItem("accounts", JSON.stringify(accounts));
+
+    console.log("Saved!");
+    location.reload();
+});
+
 // Create password
 
 
