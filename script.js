2eb2b7ff5d6ec9908eabcc1504beef841e7d6022
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-08-06 23:11:40 +0400

    assets favicon

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const { animate, hover, scroll } = Motion;
-const titlegit = document.getElementById("title-git")
-const 
+const titlegit = document.getElementById("title-git") // Lead title text to github repo
+const newpass = document.getElementById("create-pass")
+const passmodal = document.getElementById("passmodal")
 
+//Lead title text to github repo
 titlegit.addEventListener("click", (e) => {
     window.open("https://github.com/harshilarora250/lockbox/", "_blank");
 })
+// Lead title text to github repo
 
-
+// Create password
+newpass.addEventListener("click", (e) => {
+    passmodal.hidden = false;
+})
+// Create password
 
 
 
